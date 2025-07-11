a7d22f8d7e460d83ad822cde492185522099fadc
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-07-11 23:05:55 +0000

    feat: :sparkles: `GET ALL BETTER`.
    ENDPOINT USE ID ( GET ). / api.php

--- src/api.php
+++ src/api.php
@@ -31,22 +31,63 @@ switch ($recurso) {
     case 'categorias':
         switch ($method) {
             case 'GET':
-                $stmt = $pdo->prepare("SELECT * FROM categorias");
-                $stmt->execute();
-                $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                echo json_encode($response);
+                if ($id) {
+                    // Si viene un ID traer solo una categoria
+                    $stmt = $pdo->prepare("SELECT * FROM categorias WHERE id = ?");
+                    $stmt->execute([$id]);
+                    $categoria = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+                    if ($categoria) {
+                        echo json_encode($categoria);
+                    } else {
+                        http_response_code(404);
+                        echo json_encode(['error' => 'Categoria no encontrada']);
+                    }
+    
+                } else {
+                    // Si NO hay ID traer todas
+                    $stmt = $pdo->prepare("SELECT * FROM categorias");
+                    $stmt->execute();
+                    $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    echo json_encode($response);
+                }
                 break;
+            case 'POST':
+                $data = json_decode(file_get_contents('php://input'), true);
+                $stmt = $pdo->prepare("INSERT INTO categorias(name) VALUES(?)");
+                $stmt->execute([
+                    $data['name']
+                ]);
+                http_response_code(201);
+                $data['id'] = $pdo->lastInsertId();
+                echo json_encode($data);
         }
         break;
 
     case 'productos':
         switch ($method) {
             case 'GET':
-                $stmt = $pdo->prepare("SELECT * FROM productos");
-                $stmt->execute();
-                $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                echo json_encode($response);
+                if ($id) {
+                    // Si viene un ID traer solo una productos
+                    $stmt = $pdo->prepare("SELECT * FROM productos WHERE id = ?");
+                    $stmt->execute([$id]);
+                    $productos = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+                    if ($productos) {
+                        echo json_encode($productos);
+                    } else {
+                        http_response_code(404);
+                        echo json_encode(['error' => 'productos no encontrada']);
+                    }
+    
+                } else {
+                    // Si NO hay ID traer todas
+                    $stmt = $pdo->prepare("SELECT * FROM productos");
+                    $stmt->execute();
+                    $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    echo json_encode($response);
+                }
+                break;
 
         }
         break;
@@ -54,11 +95,26 @@ switch ($recurso) {
     case 'promociones':
         switch ($method) {
             case 'GET':
-                $stmt = $pdo->prepare("SELECT * FROM promociones");
-                $stmt->execute();
-                $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                if ($id) {
+                    // Si viene un ID traer solo una promociones
+                    $stmt = $pdo->prepare("SELECT * FROM promociones WHERE id = ?");
+                    $stmt->execute([$id]);
+                    $promociones = $stmt->fetch(PDO::FETCH_ASSOC);
     
-                echo json_encode($response);
+                    if ($promociones) {
+                        echo json_encode($promociones);
+                    } else {
+                        http_response_code(404);
+                        echo json_encode(['error' => 'promociones no encontrada']);
+                    }
+    
+                } else {
+                    // Si NO hay ID traer todas
+                    $stmt = $pdo->prepare("SELECT * FROM promociones");
+                    $stmt->execute();
+                    $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    echo json_encode($response);
+                }
         }
         break;
 
